3a4cde8de2d4eff5cfa4a31046be2948ac492f94
y0x3 <diegocmunoz@hotmail.com>
2024-04-21 20:26:29 -0600

    boton registro

--- app/src/main/java/com/example/limitglasses/MainActivity.kt
+++ app/src/main/java/com/example/limitglasses/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.example.limitglasses
-
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import com.example.limitglasses.SelectAccountActivity
+import com.example.limitglasses.R
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -12,13 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Encuentra el botón por su ID
+        // Encuentra los botones por sus ID
         val btnGoogle = findViewById<AppCompatButton>(R.id.btnGogle)
+        val btnGoogleR = findViewById<AppCompatButton>(R.id.btnGogleR)
 
-        // Agrega un OnClickListener al botón
+        // Agrega un OnClickListener al botón btnGoogle
         btnGoogle.setOnClickListener {
             // Crea un Intent para abrir la nueva actividad
             val intent = Intent(this, SelectAccountActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Agrega un OnClickListener al botón btnGoogleR
+        btnGoogleR.setOnClickListener {
+            // Crea un Intent para abrir la nueva actividad
+            val intent = Intent(this, btnGoogle::class.java)
             startActivity(intent)
         }
     }
